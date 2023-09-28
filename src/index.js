619540a5bc3b24ef55c7776395e091a1adae7019
Juan Carlos Vargas V <jcvargas@playvox.com>
2023-09-28 18:22:25 -0500

    feat: initial repo with sam config

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import { myLog } from "./utils/my-log.js";
+import { mapRequestToCloudfrontByCatalog } from "./utils/handle-request.js";
+
+import {
+  fetchUrlOrNull as fetchCatalogFile,
+  fetchLocalJSON,
+} from "./utils/fetch-routes.js";
+
+let frontEndCatalog = null;
+export const CDN_URI = "https://d3aoaj4kd6mi6b.cloudfront.net";
+
+export const handler = async (event) => {
+  let request = event.Records[0].cf.request;
+  myLog.info(" >>> Initial request", JSON.stringify(request));
+
+  if (request.method !== "GET") {
+    return request;
+  }
+
+  const catalog = await getFrontendCatalog();
+  request = mapRequestToCloudfrontByCatalog(request, catalog);
+
+  myLog.info(`\n >>> Final Requests >> `, JSON.stringify(request));
+  return request;
+};
+
+async function getFrontendCatalog() {
+  if (frontEndCatalog) {
+    return frontEndCatalog;
+  }
+  frontEndCatalog =
+    await fetchCatalogFile(CDN_URI) ||
+    fetchLocalJSON("../routes/frontend-catalog.json");
+  return frontEndCatalog;
+}
