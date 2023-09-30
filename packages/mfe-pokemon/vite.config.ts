10932f203b99f1af820ada8a46bfd07bd75064cb
jcvargas <jc.vargas.valencia@gmail.com>
2023-09-30 01:52:42 -0500

    feat: updating mfe edge views

--- packages/mfe-pokemon/vite.config.ts
+++ packages/mfe-pokemon/vite.config.ts
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()], 
-  base: import.meta.env.BASE_URL || "https://d1wub6s1ympoc5.cloudfront.net/mfe/pokemon/"
 })
