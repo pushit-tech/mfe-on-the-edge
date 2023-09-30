78da1e801f447a75d6729b10cdd511ed015dc7e7
jcvargas <jc.vargas.valencia@gmail.com>
2023-09-30 00:30:21 -0500

    feat: updating mfe edge views

--- packages/mfe-pokemon/vite.config.ts
+++ packages/mfe-pokemon/vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()], 
+  base: "https://d1wub6s1ympoc5.cloudfront.net/mfe/pokemon/"
   
 })
--- packages/mfe-rick-morty/vite.config.js
+++ packages/mfe-rick-morty/vite.config.js
@@ -4,4 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: 'https://d1wub6s1ympoc5.cloudfront.net/mfe/rick-morty/'
 })
