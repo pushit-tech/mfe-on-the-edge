96a431e08119f2dc19a71432f064ec4b61d27c63
jcvargas <jc.vargas.valencia@gmail.com>
2023-09-30 04:47:09 -0500

    fix: shell base

--- packages/mfe-shell/src/mfe-root-config.js
+++ packages/mfe-shell/src/mfe-root-config.js
@@ -7,7 +7,14 @@ import {
 } from "single-spa-layout";
 import microfrontendLayout from "./microfrontend-layout.html";
 
-const routes = constructRoutes(microfrontendLayout);
+const data = {
+  props: {
+    baseUrl: "/shell",
+  },
+  loaders: {},
+};
+
+const routes = constructRoutes(microfrontendLayout, data);
 const applications = constructApplications({
   routes,
   loadApp({ name }) {
